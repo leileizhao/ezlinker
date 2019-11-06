2302c81e8e5ad0b190ed6ea76ab1f5dc137f27c8
wwhai <751957846@qq.com>
2019-11-06 16:55:35 +0800

    错别字：future->feature

--- app/src/main/java/com/ezlinker/app/AppApplication.java
+++ app/src/main/java/com/ezlinker/app/AppApplication.java
@@ -1,11 +1,9 @@
 package com.ezlinker.app;
 
-import com.ezlinker.common.exchange.R;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
