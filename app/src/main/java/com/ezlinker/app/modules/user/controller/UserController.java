9e020b83cac51c93a9dec632c3e5b3a4be30296f
wwhai <751957846@qq.com>
2019-11-15 11:46:29 +0800

    用户资料更新

--- app/src/main/java/com/ezlinker/app/modules/user/controller/UserController.java
+++ app/src/main/java/com/ezlinker/app/modules/user/controller/UserController.java
@@ -7,13 +7,16 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ezlinker.app.common.AbstractXController;
 import com.ezlinker.app.modules.user.form.ResetPasswordForm;
+import com.ezlinker.app.modules.user.form.UserUpdateForm;
 import com.ezlinker.app.modules.user.model.User;
 import com.ezlinker.app.modules.user.service.IUserService;
 import com.ezlinker.app.modules.userlog.model.UserLoginLog;
 import com.ezlinker.app.modules.userlog.service.IUserLoginLogService;
 import com.ezlinker.common.exception.XException;
 import com.ezlinker.common.exchange.R;
+import com.ezlinker.common.utils.RegxUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -92,6 +95,47 @@ public class UserController extends AbstractXController<User> {
             throw new XException("Invalid password!", "旧密码错误");
         }
         user.setPassword(SecureUtil.md5(form.getNewPassword()));
+        boolean ok = iUserService.updateById(user);
+        return ok ? success() : fail();
+
+    }
+
+    /**
+     * 更新用户信息
+     *
+     * @param form
+     * @return
+     * @throws XException
+     */
+    @PutMapping("/info")
+    public R updateInfo(UserUpdateForm form) throws XException {
+        User user = iUserService.getById(getUserDetail().getId());
+        if (!StringUtils.isEmpty(form.getPhone())) {
+            if (!RegxUtil.isPhone(form.getPhone())) {
+                throw new XException("Invalid phone format!", "手机号码错误");
+
+            } else {
+                user.setPhone(form.getPhone());
+
+            }
+
+        }
+        if (!StringUtils.isEmpty(form.getRealName())) {
+            user.setRealName(form.getRealName());
+
+        }
+        if (!StringUtils.isEmpty(form.getNickName())) {
+            user.setNickName(form.getNickName());
+
+        }
+        if (!StringUtils.isEmpty(form.getEmail())) {
+            if (!RegxUtil.isEmail(form.getEmail())) {
+                throw new XException("Invalid email format!", "邮箱格式错误");
+            } else {
+                user.setEmail(form.getEmail());
+
+            }
+        }
         boolean ok = iUserService.updateById(user);
         return ok ? success() : fail();
 
