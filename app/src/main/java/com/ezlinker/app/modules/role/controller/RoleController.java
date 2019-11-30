2273a40afd5b997f122abd9cfb69ea71d8a5f9ce
zhaolei <zhaolei@thunk.cn>
2019-11-30 16:35:08 +0800

    :sparkles: :construction: 新增角色管理,权限管理

--- app/src/main/java/com/ezlinker/app/modules/role/controller/RoleController.java
+++ app/src/main/java/com/ezlinker/app/modules/role/controller/RoleController.java
@@ -1,31 +1,108 @@
 package com.ezlinker.app.modules.role.controller;
 
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ezlinker.app.modules.role.model.Role;
+import com.ezlinker.app.modules.role.service.IRoleService;
 import com.ezlinker.common.exception.XException;
 import com.ezlinker.common.exchange.R;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import com.ezlinker.app.common.AbstractXController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
- * <p>
- * 用户角色 前端控制器
- * </p>
+ * <p>Title: RoleController</p>
+ * <p>Description: com.ezlinker.app.modules.role.controller</p>
  *
- * @author wangwenhai
- * @since 2019-11-11
+ * @author zhaolei
+ * @version 1.0.0
+ * @date 2019-11-30 16:04
  */
 @RestController
 @RequestMapping("/role")
 public class RoleController extends AbstractXController<Role> {
 
-    public RoleController(HttpServletRequest httpServletRequest) {
+    private final IRoleService iRoleService;
+
+    public RoleController(HttpServletRequest httpServletRequest, IRoleService iRoleService) {
         super(httpServletRequest);
+        this.iRoleService = iRoleService;
     }
 
+    /**
+     * 添加一个角色
+     *
+     * @param role
+     * @return
+     */
+    @Override
+    protected R add(@RequestBody Role role) {
+        return iRoleService.save(role) ? success() : fail();
+    }
+
+    /**
+     * 更新角色
+     *
+     * @param id
+     * @param role
+     * @return
+     */
+    @Override
+    protected R update(@PathVariable Long id, @RequestBody Role role) throws XException {
+        role.setId(id);
+        return iRoleService.updateById(role) ? success() : fail();
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @Override
+    protected R delete(@PathVariable Integer[] ids) {
+        return iRoleService.removeByIds(Arrays.asList(ids)) ? success() : fail();
+    }
+
+    /**
+     * 查询单个
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    protected R get(@PathVariable Long id) {
+        return data(iRoleService.getById(id));
+    }
+
+    /**
+     * 查询列表
+     * @param label
+     * @param name
+     * @param parent
+     * @param current
+     * @param size
+     * @return
+     */
+    @GetMapping
+    public R getRoleList(@RequestParam(value = "label", required = false) String label,
+                         @RequestParam(value = "name", required = false) String name,
+                         @RequestParam(value = "parent", required = false) Integer parent,
+                         @RequestParam(value = "current", required = false, defaultValue = "1") Integer current,
+                         @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+        QueryWrapper<Role> queryWrapper = new QueryWrapper<Role>()
+                .like(StrUtil.isNotBlank(label), "label", label)
+                .like(StrUtil.isNotBlank(name), "name", name)
+                .eq(parent != null, "parent", parent)
+                .orderByDesc("create_time");
+        return data(iRoleService.page(new Page<>(current, size), queryWrapper));
+    }
 }
 
